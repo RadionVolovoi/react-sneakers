49382a57b053ce403371b2c10346bd0d57178e10
Radion Volovoi <Radikvolovoy@gmail.com>
2023-05-01 10:23:46 +0300

    addedState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import "./App.scss";
+import React from "react";
 import Card from "./components/Card";
-import Drawer from "./components/Drawer";
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://6447ed9c50c2533744339a45.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    setCartItems((prev) => [...prev, obj]);
+  };
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>All Sneakers</h1>
+          <h1>All sneakers</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Search..." />
+            <input placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          <Card />
+
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log("Добавили в закладки")}
+              onPlus={(obj) => onAddToCart(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,57 +1,48 @@
-import React from "react";
-
-const Drawer = () => {
+function Drawer({ onClose, items = [] }) {
   return (
-    <div style={{ display: "none" }} className="overlay">
+    <div className="overlay">
       <div className="drawer">
-        <h2 className="d-flex  justify-between mb-30">
-          Shop <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove" />
+        <h2 className="d-flex justify-between mb-30">
+          Shop <img onClick={onClose} className="cu-p" src="/img/btn-remove.svg" alt="Close" />
         </h2>
 
         <div className="items">
-          <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-              className="cartItemImg"
-            ></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>199$</b>
+          {items.map((obj) => (
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                className="cartItemImg"
+              ></div>
+
+              <div className="mr-20 flex">
+                <p className="mb-5">{obj.title}</p>
+                <b>{obj.price} $</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
             </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-          </div>
-          <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-              className="cartItemImg"
-            ></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>199$</b>
-            </div>
-            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-          </div>
-          <div className="cartTotalBlock">
-            <ul className="cartTotalBlock">
-              <li>
-                <span>Total:</span>
-                <div></div>
-                <b>398$</b>
-              </li>
-              <li>
-                <span>Tax 5% :</span>
-                <div></div>
-                <b>19$</b>
-              </li>
-            </ul>
-            <button className="greenButton">
-              Сonfirm order <img src="/img/arrow.svg" alt="Arrow" />
-            </button>
-          </div>
+          ))}
+        </div>
+
+        <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Total:</span>
+              <div></div>
+              <b>21 498 $ </b>
+            </li>
+            <li>
+              <span>Tax 5%:</span>
+              <div></div>
+              <b>1074 $. </b>
+            </li>
+          </ul>
+          <button className="greenButton">
+            Create order <img src="/img/arrow.svg" alt="Arrow" />
+          </button>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Drawer;
